fc839c63d69a0aef810bd6ce83ba8edf7ed8f01f
pubudhi <pywitharana@gmail.com>
2026-05-16 03:42:41 +0530

    Connected frontend to eks loadbalancer service

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 export const profileAPI = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: "http://ab01531058c9d44d19c579612b3e07f2-1982258502.ap-southeast-1.elb.amazonaws.com:3001"
 });
 
 export const bookingAPI = axios.create({
-  baseURL: "http://localhost:3002"
+  baseURL: "http://ade162663129b40e79491fd25306783e-241988909.ap-southeast-1.elb.amazonaws.com:3002"
 });
 
 export const submissionAPI = axios.create({
-  baseURL: "http://localhost:3003"
+  baseURL: "http://a0a842368e2b244bca3f05ca51034018-1100873480.ap-southeast-1.elb.amazonaws.com:3003"
 });
